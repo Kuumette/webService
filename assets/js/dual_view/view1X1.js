0f675ea4b49e88ad517e10456f87d5a476c3e2e5
unknown <ballatoremanon@gmail.com>
2022-11-30 18:04:15 +0100

    pour web service

--- assets/js/dual_view/view1X1.js
+++ assets/js/dual_view/view1X1.js
@@ -2,6 +2,6 @@ const VIEW_SAMPLE = document.querySelectorAll("#viewSample");
 
 VIEW_SAMPLE.forEach((view) => {
 	view.addEventListener("click", () => {
-		document.location.href = "./viewSimple.html";
+		document.location.href = "./index.html";
 	});
 });
